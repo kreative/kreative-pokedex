0ec85b4c093335aa0fcfcb34c78dff7da7c96a19
Ryan Huellen <ryan@huellenconsulting.com>
2023-09-30 17:41:50 -0500

    fix: no border

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,7 +11,7 @@ export const Card = ({ name, description, imageUrl, color }: CardProps) => {
       style={{
         backgroundColor: color,
       }}
-      className='flex w-64 flex-col items-center justify-center rounded border p-4 shadow-md'>
+      className='flex w-64 flex-col items-center justify-center rounded p-4 shadow-md'>
       <img className='h-64' src={imageUrl} alt={name} />
       <h3 className='text-3xl'>{name}</h3>
       <p className='text-xl'>{description}</p>
